1b3feb316f0f04e164349880dd18855c179fe272
Casora Zitora <cqz21@mails.tsinghua.edu.cn>
2023-04-09 00:55:08 +0800

    fix turing's method
    add hand-computed cycles

--- turing.py
+++ turing.py
@@ -4,9 +4,10 @@ from typing import List, Tuple
 from enigma import Enigma
 
 
-def decypher(loops: List[Tuple[List[int], chr]]):
+def decypher(loops: List[List[int]]):
     possibilities = []
     for rotor_order in [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]:
+    # for rotor_order in [[2, 3, 1]]:
         print("Rotor order: %d-%d-%d" % tuple(rotor_order), end='')
         print(", %d possibilities found" % len(possibilities))
         for i in range(26):
@@ -20,9 +21,8 @@ def decypher(loops: List[Tuple[List[int], chr]]):
                     for ch in range(26):
                         flag = True
                         ch = chr(ch + ord('A'))
+
                         for loop in loops:
-                            origin_ch = loop[1]
-                            loop = loop[0]
                             prev_fin = ch
                             for cnt in loop:
                                 enigma.reset()
@@ -30,19 +30,20 @@ def decypher(loops: List[Tuple[List[int], chr]]):
                             if prev_fin != ch:
                                 flag = False
                                 break
+
                         if flag:
-                            possibilities.append((rotor_order, init_rotor, origin_ch, ch))
+                            possibilities.append((rotor_order, init_rotor, ch))
         print()
     return possibilities
 
 
 if __name__ == '__main__':
     res = decypher([
-        ([0, 9], 'A'),
-        ([2, 3], 'T'),
-        ([4, 7, 11], 'B'),
-        ([6, 7, 8, 1, 16], 'D'),
-        ([10, 12, 7, 8, 9], 'A'),
+        [7, 8, 1, 16, 6],
+        [7, 11, 4],
+        [7, 8, 9, 10, 12],
+        [7, 8, 1, 2, 3, 16, 6],
+        [7, 8, 9, 0, 1, 16, 6],
     ])
     with open('turing.json', mode='w', encoding='utf-8') as fo:
         json.dump(res, fo)
