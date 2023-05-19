62a3a64baad3de8998ef8252cec8f8fc00b36085
Casora Zitora <cqz21@mails.tsinghua.edu.cn>
2023-05-19 10:04:19 +0800

    fix bugs

--- aes.cpp
+++ aes.cpp
@@ -18,7 +18,7 @@ unsigned char* aes::encrypt(string& original_text, unsigned char** iv) {
     mt19937 gen(rd());
     uniform_int_distribution<> distr(0, 255);
     *iv = new unsigned char[16];
-    for (int i = 0; i < 16; i++) *iv[i] = (unsigned char)distr(gen);
+    for (int i = 0; i < 16; i++) (*iv)[i] = (unsigned char)distr(gen);
 
     for (unsigned long long i = 0; 16 * i < len; i++) {
         auto transcripted = new unsigned char[16];
@@ -32,17 +32,22 @@ unsigned char* aes::encrypt(string& original_text, unsigned char** iv) {
         if (i == 0) round_keys[0] = new block(*iv);  // for the first block use iv as the first round_key
         else round_keys[0] = new block(*ans.back()); // else use the last encrypted block
         auto w = new unsigned int[44];  // in slides' fashion
-        for (int j = 0; j < 4; j++) w[j] = *iv[j];
+        for (int j = 0; j < 4; j++) w[j] = (*iv)[j];
         for (int j = 4; j < 44; j++) {
-            auto temp = w[j - 1];
-            if (j % 4 == 0) temp = S_BOX[(temp << 2) + (temp >> 6)] ^ RCON[(int)(j / 4)];
+            unsigned int temp = w[j - 1];
+            if (j % 4 == 0) {
+                // temp = S_BOX[(temp << 8) + (temp >> 24)];
+                temp = S_BOX[temp >> 24] + (S_BOX[temp & 0xFF] << 8)
+                        + (S_BOX[(temp >> 8) & 0xFF] << 16) + (S_BOX[(temp >> 16) & 0xFF] << 24);
+                temp = temp ^ RCON[(int)(j / 4)];
+            }
             w[j] = w[j - 4] ^ temp;
         }
 
         // CBC implementation
-        curr_state->xor_with(*round_keys[0]);
-        for (int round = 0; round < 9; round++) curr_state->step_through(*round_keys[round]);
-        curr_state->step_through(*round_keys[10], false);
+        curr_state->xor_with((*round_keys)[0]);
+        for (int round = 0; round < 9; round++) curr_state->step_through((*round_keys)[round]);
+        curr_state->step_through((*round_keys)[10], false);
 
         // save to vector `ans`
         ans.emplace_back(curr_state);
--- main.cpp
+++ main.cpp
@@ -6,4 +6,11 @@ using namespace std;
 
 
 int main() {
+    unsigned char* key;
+    string to_encrypt = "Shall I compare thee to a summer's day / Thou art more lovely and more temperate";
+    auto result = aes::encrypt(to_encrypt, &key);
+    for (int i = 0; i < 16; i++) cout << hex << (unsigned short)key[i] << " ";
+    cout << endl;
+    for (int i = 0; i < to_encrypt.length(); i++) cout << hex << (unsigned short)result[i] << " ";
+    return 0;
 }
